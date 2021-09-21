c5200274771d5cdc75c720919b1e20bbca44aff5
Wallos <stepan.zamazal@seznam.cz>
2021-09-21 06:52:43 +0200

    devstack
    starting

---
+++ components/styledComponents/styledComponents.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    border: none;
+    color: blue;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+`
