4108a6685cef65399f806f2c013527a31fdb3fd8
Wallos <stepan.zamazal@seznam.cz>
2021-12-13 18:08:48 +0100

    Registration with users-coll create

--- components/auth/RegisterForm.tsx
+++ components/auth/RegisterForm.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React , { useState, useEffect } from 'react'
+import {createUserWithEmailAndPassword} from "firebase/auth"
+import{auth} from "../../utils/firebase"
+import db from "../../utils/firebase"; 
+import Router from 'next/router'
 import {Formik, Form} from "formik"
 import {TextField} from '../TextField'
 import * as Yup from 'yup'
+import { addDoc, collection, doc, setDoc } from 'firebase/firestore'
 export const RegisterForm = () => {
+    //const[userUID, setUserUID] = useState<string>("");
+    const register = async (values:any) =>{
+        try{
+            const user = await createUserWithEmailAndPassword(auth, values.email, values.password);
+            if(user.user.uid.length > 0){
+                AddData(values, user.user.uid);
+            }
+        }catch(error : any){
+            console.log(error.message)
+        }
+        
+    }
+    const AddData = async (values:any, userUID : string) =>{
+        try {
+            const docRef = await addDoc(collection(db, "users"), {
+                id: userUID,
+                email: values.email,
+                firstName: values.firstName,
+                lastName: values.lastName,
+                nickname: values.nickname,
+            });
+          
+            console.log("Document written with ID: ", userUID);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
+    }
     const validate = Yup.object({
         firstName: Yup.string().max(15,"Must be 15 character or less").required('Required'),
         lastName: Yup.string().max(20,"Must be 20 character or less").required('Required'),
@@ -23,13 +55,14 @@ export const RegisterForm = () => {
             }}
             validationSchema={validate}
             onSubmit={values =>{
-                console.log(values);
+                register(values);
+                //console.log("xxx",userUID, "xxx");
+                //AddData(values);
             }}
         >
             {formik =>(
                 <div>
                     <h1>Register</h1>
-                    {console.log(formik.values)}
                     <Form>
                         <TextField label="First Name" name="firstName" type="text" />
                         <TextField label="Last Name" name="lastName" type="text" />
@@ -42,7 +75,6 @@ export const RegisterForm = () => {
                     </Form>
                 </div>
             )
-
             }
         </Formik>
     )
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,9 +1,42 @@
-import {RegisterForm} from "../components/auth/RegisterForm";
-import { LoginForm} from "../components/auth/LoginForm"
+import React , { useState, useEffect } from 'react'
+import {createUserWithEmailAndPassword} from "firebase/auth"
+import{auth} from "../utils/firebase"
 const Test = () => {
+    const[registerEmail, SetRegisterEmail] = useState("");
+    const[registerPassword, SetRegisterPassword] = useState("");
+    const register = async () =>{
+        try{
+            const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+            console.log(user.user.uid)
+        }catch(error : any){
+            console.log(error.message)
+        }
+        
+    }
     return(
         <div>
-            <LoginForm/>
+            <div>
+                <h3>Register</h3>
+                <input 
+                    placeholder="Email..." 
+                    onChange={(event)=>{
+                        SetRegisterEmail(event.target.value)
+                    }}
+                required>
+
+                </input>
+                <input 
+                    placeholder="Password..."
+                    onChange={(event)=>{
+                        SetRegisterPassword(event.target.value)
+                    }}
+                required>
+
+                </input>
+                
+
+                <button onClick={register}>CreateUser</button>
+            </div>
         </div>
     )
 }
