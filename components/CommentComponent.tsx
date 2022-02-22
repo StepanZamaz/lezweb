85eeb988df42593538125b64869bd57e4e82b9bc
Wallos <stepan.zamazal@seznam.cz>
2022-02-22 21:55:44 +0100

    Render of comments

--- components/CommentComponent.tsx
+++ components/CommentComponent.tsx
@@ -1,13 +1,12 @@
 import { Formik, Form } from 'formik'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import * as Yup from 'yup'
 import CommentTextField from './formikFields/CommentTextField'
 import { useRouter } from "next/router";
-import { addDoc,doc, DocumentData, setDoc, Timestamp, arrayUnion } from 'firebase/firestore'
+import { addDoc, doc, DocumentData, setDoc, Timestamp, arrayUnion, onSnapshot, collection } from 'firebase/firestore'
 import db from "../utils/firebase";
 import { getAuth } from 'firebase/auth'
-
 const CommentSection = styled.div`
     
 `
@@ -29,25 +28,44 @@ const ButtonContainer = styled.div`
 const AddCommentBtn = styled.button`
     
 `
+const CommentDiv = styled.div`
+    border: 2px solid black;
+    width: 50%;
+    margin: 0;
+`
 const CommentComponent = () => {
     const auth = getAuth();
     const router = useRouter();
+    const [comments, setComments] = useState<DocumentData>({})
+    console.log("comments", comments)
     const { route } = router.query;
+    useEffect(() => {
+        console.log("ahoj")
+        onSnapshot(collection(db, "comments"), snapshot => {
+            const data = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            console.log("data", data)
+            const listedData = data?.find(x => x.id === route);
+            if (listedData) {
+                setComments(listedData);
+            }
+        })
+    }, [route]);
     const validate = Yup.object({
         commentText: Yup.string().max(100, "Maximálně 100 znaků").required('Vyžadováno'),
     })
     var generateID = () => {
         return '_' + Math.random().toString(36).substr(2, 9);
     };
+
     const addComment = async (data: DocumentData) => {
         //@ts-ignore
         let routeId: string = route;
-        
+
         const commentRef = doc(db, "comments", routeId);
         await setDoc(commentRef, {
             comment: arrayUnion(
                 {
-                    commentId : generateID(),
+                    commentId: generateID(),
                     commentText: data.commentText,
                     addedBy: auth.currentUser?.uid,
                     addDate: Timestamp.fromDate(new Date())
@@ -59,7 +77,35 @@ const CommentComponent = () => {
         <CommentSection>
             <CommentHeading>Komentáře</CommentHeading>
             <DisplayComment>
+                {
+                    comments.comment === undefined ? (
+                        <div>
+                            no comment
+                        </div>
 
+                    ) : (
+                        <>
+                            {
+                                Object.keys(comments.comment).map((key) => {
+                                    const singleComment = comments.comment[key];
+                                    //const date = singleComment.addDate;
+                                    console.log("x", comments.comment[key]);
+                                    return (
+                                        <CommentDiv>
+                                            <ul>
+                                                <li>datum: { }</li>
+                                                <li>idUser: {singleComment.addedBy}</li>
+                                                <li>idComment: {singleComment.commentId}</li>
+                                                <li>commentText: {singleComment.commentText}</li>
+                                            </ul>
+                                        </CommentDiv>
+                                    )
+                                })
+                            }
+                        </>
+                    )
+
+                }
             </DisplayComment>
             <DivInputComment>
                 <Formik
